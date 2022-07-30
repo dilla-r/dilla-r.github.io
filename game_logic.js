32daf44596e13093ffeb768b44c44a640b84bb2e
Rebecca Dilla <rebeccadilla814@gmail.com>
2022-07-30 16:58:30 -0700

    Update game_logic.js

--- game_logic.js
+++ game_logic.js
@@ -1,1 +1,6 @@
-document.getElementById("answer").innerHTML = 5 + 6;
+var currentTime = new Date()
+var specialTime = new Date(2022,07,30)
+var myTume = new Date(1993,08,24)
+
+if (currentTime == specialTime) {
+document.getElementById("answer").innerHTML = Yes!;}
