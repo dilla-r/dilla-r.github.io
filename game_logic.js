ff364ae5f592677c144576365392d23b83364dcd
Rebecca Dilla <rebeccadilla814@gmail.com>
2022-07-30 18:24:49 -0700

    Update game_logic.js

--- game_logic.js
+++ game_logic.js
@@ -1,14 +1,14 @@
-var currentTime = new Date()
-var weirdTime = currentTime
-var specialTime = new Date(2022,07,30)
-var myTime = new Date(1993,08,24)
+var currentTime = new Date();
+var weirdTime = currentTime;
+var specialTime = new Date(2022,07,30);
+var myTime = new Date(1993,08,24);
 const gamez = [
 "2022-07-30",
   "2022-07-31",
   "2022-08-02",
   "2022-08-03",
   "2022-08-04",
-  "2022-08-13"
+  "2022-08-13",
   "2022-08-16",
   "2022-08-17",
   "2022-08-18",
@@ -19,7 +19,7 @@ const gamez = [
   "2022-08-23",
   "2022-08-24",
   "2022-08-28"
-]
+];
 
 if (specialTime == currentTime.setHours(0,0,0,0)) {
 document.getElementById("answer").innerHTML = "Yas";
