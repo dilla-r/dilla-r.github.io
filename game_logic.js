f8d6f07bd11f6ffc2e999f1838a53ac5e686767d
Rebecca Dilla <rebeccadilla814@gmail.com>
2022-07-30 18:04:10 -0700

    Update game_logic.js

--- game_logic.js
+++ game_logic.js
@@ -1,6 +1,6 @@
 var currentTime = new Date()
 var weirdTime = currentTime
-var specialTime = new Date(2022-07-30)
+var specialTime = new Date(2022,07,30)
 var myTime = new Date(1993,08,24)
 const gamez = [
 "2022-07-30",
