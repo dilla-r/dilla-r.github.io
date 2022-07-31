a5c024e612f625b7ee9e9ebef84b1957a87a7ae8
Rebecca Dilla <rebeccadilla814@gmail.com>
2022-07-30 17:26:52 -0700

    Update game_logic.js

--- game_logic.js
+++ game_logic.js
@@ -3,6 +3,6 @@ var weirdTime = currentTime
 var specialTime = new Date(2022,07,30)
 var myTime = new Date(1993,08,24)
 
-if (1 < 2) {
+if (weirdTime == currentTime) {
 document.getElementById("answer").innerHTML = "Yes!";
 }
